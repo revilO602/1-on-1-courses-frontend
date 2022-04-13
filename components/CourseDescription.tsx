660ba00d989806767f6714a548cc3510bd10ec46
Pucko99 <marekoravec4@gmail.com>
2022-04-13 15:37:02 +0200

    Added the timetable component to the CourseDescription, but not yet with real data.

--- components/CourseDescription.tsx
+++ components/CourseDescription.tsx
@@ -1,35 +1,77 @@
 import React from 'react';
-import {Text, View, StyleSheet} from 'react-native';
+import {Text, View, StyleSheet, ScrollView} from 'react-native';
 import Colors from "../constants/Colors";
+import TimeTableView, { genTimeBlock } from 'react-native-timetable';
+
+
+const events_data = [
+    {
+        title: "Math",
+        startTime: genTimeBlock("MON", 9),
+        endTime: genTimeBlock("MON", 10, 50),
+        location: "Classroom 403",
+        extra_descriptions: ["Kim", "Lee"],
+    },
+    {
+        title: "Math",
+        startTime: genTimeBlock("WED", 9),
+        endTime: genTimeBlock("WED", 10, 50),
+        location: "Classroom 403",
+        extra_descriptions: ["Kim", "Lee"],
+    },
+    {
+        title: "Physics",
+        startTime: genTimeBlock("MON", 11),
+        endTime: genTimeBlock("MON", 11, 50),
+        location: "Lab 404",
+        extra_descriptions: ["Einstein"],
+    }
+];
 
 export default function CourseDescription({navigation, route}){
     // console.log('------------------------------------------')
     // console.log(route.course.id);
     return (
-        <View style={styles.container}>
-            <View style={styles.viewStyle}>
-                <Text style={styles.titleStyle}>{route.course.name}</Text>
-                <Text style={styles.subtitleStyle}>by {route.course.teacher.firstName} {route.course.teacher.lastName}</Text>
-            </View>
-            <View style={styles.viewStyle}>
-                <Text style={styles.descriptionStyle}>Description</Text>
-                <Text>{route.course.description}</Text>
-            </View>
+        <ScrollView contentContainerStyle={{backgroundColor: Colors.background, margin: 5}}>
+            {/*<View style={styles.container}>*/}
+                <View style={styles.viewStyle}>
+                    <Text style={styles.titleStyle}>{route.course.name}</Text>
+                    <Text style={styles.subtitleStyle}>by {route.course.teacher.firstName} {route.course.teacher.lastName}</Text>
+                </View>
+                <View style={styles.viewStyle}>
+                    <Text style={styles.descriptionStyle}>Description</Text>
+                    <Text style={{margin: 5}}>{route.course.description}</Text>
+                </View>
+                <View style={styles.viewStyle}>
+                    <TimeTableView
+                        //scrollViewRef={this.scrollViewRef}
+                        events={events_data}
+                        pivotTime={9}
+                        //pivotDate={this.pivotDate}
+                        numberOfDays={7}
+                        //onEventPress={this.onEventPress}
+                        //headerStyle={styles.headerStyle}
+                        formatDateHeader="dddd"
+                        locale="en-US"/>
+                </View>
 
-        </View>
+            {/*</View>*/}
+        </ScrollView>
     );
 }
 
 const styles = StyleSheet.create({
     titleStyle: {
-        fontSize: 30,
+        fontSize: 25,
         color: Colors.text,
         fontWeight: 'bold',
+        margin: 5,
     },
     subtitleStyle: {
         fontSize: 15,
         color: Colors.text,
         fontWeight: 'bold',
+        margin: 5,
     },
     container: {
         // flex: 1,
@@ -43,6 +85,7 @@ const styles = StyleSheet.create({
         fontSize: 22,
         color: Colors.text,
         fontWeight: 'bold',
+        margin: 5,
     },
     viewStyle: {
         marginBottom: 5,
